52505468e00a01be560ac97b2d311e6838cb2253
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-12-11 17:28:04 +0100

    ciqual: add all app files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+﻿var react = require('react');
+var App = require('./ingredients/ingredientsPage.js');
+var appDispatcher = require('./appDispatcher.js');
+
+function main() {
+    appDispatcher.initializeApp();
+    react.renderComponent(App(), document.getElementById("content"));
+}
+
+window.onload = main;
